bbbc3da89f3b9e28fea0cd9b50617a6e3a4dcaf1
unknown <you@example.com>
2018-06-24 12:08:21 +0300

    localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import App from './App';
+import list from '../data/list.json'
+const {posts} = list
+localStorage.setItem('posts', JSON.stringify(posts));
 
 ReactDOM.render(
   <App />,
